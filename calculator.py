9e712df21c8be9313d5ac8c31a706a9c3e188ecf
vagrant <vagrant@vagrant.vm>
2020-09-30 20:15:42 +0000

    prompts user and calls add function

--- calculator.py
+++ calculator.py
@@ -3,5 +3,18 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+while True:
+    user_input = input("> ")
+    tokens = user_input.split(" ")
+    
+    if tokens[0] == "q":
+        print("Thanks for using our calculator")
+        break
+    else:
+        num1 = float(tokens[1])
+        num2 = float(tokens[2])
+        if tokens[0] == "+":
+            print(add(num1, num2))
+            
 
 # Replace this with your code
