e34b6fc68dc2bcf6c8878cf0ad841d3a97889a8b
vagrant <vagrant@vagrant.vm>
2020-09-30 20:32:58 +0000

    finished main section

--- calculator.py
+++ calculator.py
@@ -12,9 +12,27 @@ while True:
         break
     else:
         num1 = float(tokens[1])
-        num2 = float(tokens[2])
+        try:
+            num2 = float(tokens[2])
+        except: 
+            num2 = 0
         if tokens[0] == "+":
             print(add(num1, num2))
+        elif tokens[0] == "-":
+            print(subtract(num1, num2))
+        elif tokens[0] == "*":
+            print(multiply(num1, num2))
+        elif tokens[0] == "/":
+            print(divide(num1, num2))
+        elif tokens[0] == "square":
+            print(square(num1))
+        elif tokens[0] == "cube":
+            print(cube(num1))
+        elif tokens[0] == "pow":
+            print(power(num1, num2))
+        elif tokens[0] == "mod":
+            print(mod(num1, num2))
+
             
 
 # Replace this with your code
